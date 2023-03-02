35bbf4fd7bf28f1e301d9827e295ef9612269e43
ridmi-balasooriya <ridmi.bizy@gmail.com>
2023-03-02 19:38:52 +0400

    Adding Routes to load the pages update routes in app.js and add navigation bar. Add url parameter to ArticlePage.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesListPage from './pages/ArticlesListPage';
+import ArticlePage from './pages/ArticlePage';
+import NavBar from './NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-        <h1>My React Blog</h1>
-        <div id='page_body'>
-          Welcome to my react blog.
-        </div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <NavBar />
+          <div id='page_body'>
+              <Routes>
+                <Route path='/' element={<HomePage />} /> 
+                <Route path='/about-us' element={<AboutPage />} />
+                <Route path='/articles' element={<ArticlesListPage />} />
+                <Route path='/articles/:articleId' element={<ArticlePage />} />
+              </Routes>
+          </div>
+      </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,20 @@
+import { useParams } from "react-router-dom";
+import articles from "./article-content";
+
+const ArticlePage = () => {
+    const params = useParams();
+    const { articleId } = params;   
+    const article = articles.find( (article) => article.name === articleId);
+
+    return(
+
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph) => (
+                <p key={paragraph}>{paragraph}</p>
+            ))}
+        </>
+    );
+}
+
+export default ArticlePage;
