069547769fc24312d60af3b2275b2a8dfb757e7f
ridmi-balasooriya <ridmi.bizy@gmail.com>
2023-03-08 10:02:20 +0400

    Impletement NotFoundPage.js and Update ArticlePage for not found state

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,10 +1,15 @@
 import { Link, useParams } from "react-router-dom";
 import articles from "./article-content";
+import NotFoundPage from "./NotFoundPage";
 
 const ArticlePage = () => {
     const params = useParams();
     const { articleId } = params;   
     const article = articles.find( (article) => article.name === articleId);
+
+    if(!article){
+        return <NotFoundPage />
+    }
 
     return(
         <>
