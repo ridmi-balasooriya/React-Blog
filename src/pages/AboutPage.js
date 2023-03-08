f161c4f3dc3f53f878e094d0393c01fe9ae5e623
ridmi-balasooriya <ridmi.bizy@gmail.com>
2023-03-08 09:51:36 +0400

    Updated About Us page and Home Page Content

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,17 @@
 const AboutPage = () => {
     return(
-        <h1>This is the About page</h1>
+        <>
+            <h1>About Me</h1>
+            <p>
+                Suspendisse et sagittis mauris. Sed pharetra arcu ac quam vestibulum rutrum. Fusce vestibulum nulla augue, et sagittis mauris rhoncus ut. Suspendisse sollicitudin vitae felis quis sagittis. Duis egestas dignissim justo eget dapibus. Mauris porta vel massa id porta. Donec venenatis odio eget viverra maximus. Vivamus fermentum elementum nisl, sit amet elementum sapien cursus quis.
+            </p>
+            <p>
+                Aenean feugiat mi malesuada urna pulvinar iaculis. Maecenas at pharetra felis. Sed hendrerit eget purus eu vulputate. Suspendisse fringilla maximus diam, vel luctus turpis convallis vel. Fusce ligula diam, hendrerit sed libero ac, commodo gravida nunc. Praesent eros est, condimentum in sodales sed, consequat eget ipsum. Nullam pharetra nulla neque, vitae cursus turpis aliquet at. Pellentesque ut dui vel mauris posuere suscipit. Vestibulum eget quam dui. Morbi nec dolor at elit lobortis malesuada. Sed posuere aliquam vulputate. Sed eu nibh blandit, sollicitudin ex in, iaculis lectus.
+            </p>
+            <p>
+                Curabitur nec fringilla mauris, in placerat nibh. Vivamus imperdiet enim vitae lacinia maximus. Pellentesque cursus, velit sed consectetur gravida, justo nisi fringilla felis, et feugiat sem ex eu dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce in ipsum non dui commodo scelerisque at id turpis. Curabitur id tellus posuere, malesuada metus ut, molestie tortor. Nullam sem dui, ullamcorper quis tortor eu, maximus fringilla diam. Ut sodales maximus sapien, quis tincidunt urna dignissim quis. Nam a auctor lacus. Donec convallis, magna ac tincidunt luctus, mauris nulla pretium leo, non finibus nibh sapien a neque. Cras est elit, sagittis sed porta sit amet, scelerisque at enim. Vivamus eget commodo ligula. Praesent vestibulum tellus ante, eget varius est congue id. Nullam orci nunc, semper ac tellus vel, placerat dictum augue.
+            </p>
+        </>
     );
 }
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,17 @@
 const HomePage = () => {
     return(
-        <h1>This is the Home page</h1>
+        <>
+            <h1>Hello, welcome to my blog!</h1>
+            <p>
+                Maecenas sagittis placerat libero, tempor porta diam euismod eget. Suspendisse lobortis metus at nisi ornare, et ultricies ligula scelerisque. Vestibulum euismod consequat odio vitae volutpat. Pellentesque non accumsan nulla. Nunc dignissim cursus diam, rhoncus semper orci maximus nec. Sed vestibulum turpis nec diam imperdiet, ut fermentum velit fringilla. Ut sit amet ipsum enim. Nullam non massa eu sapien accumsan eleifend. Integer at mauris ultrices, efficitur nulla in, fringilla quam. Sed bibendum pretium mi. Nullam eget mi leo. Integer sit amet pharetra justo, non luctus metus. Etiam fermentum faucibus mi in iaculis. Nulla vel fermentum nisl.
+            </p>
+            <p>
+                Sed fermentum quam id dolor placerat fringilla. Proin luctus mattis massa, nec blandit justo dignissim a. Cras a dictum justo. Integer rutrum dui eu ornare vulputate. Suspendisse nec eros et nisl ullamcorper eleifend. Nunc consequat risus odio. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse tempor neque in sagittis rutrum. Duis eleifend ornare enim, vehicula sollicitudin magna sodales vel. Nam felis elit, euismod nec enim vel, volutpat fermentum libero. Aenean fermentum arcu et condimentum malesuada. Fusce interdum lacinia sagittis. Quisque ut ante at tortor faucibus volutpat. Nulla egestas iaculis quam, eget blandit tellus. Mauris lobortis iaculis mauris eu tincidunt. Mauris eu gravida massa, sed maximus magna.
+            </p>
+            <p>
+                Maecenas aliquam justo metus, nec malesuada tellus molestie vel. Pellentesque accumsan libero sit amet placerat fringilla. Nulla facilisis quis erat ac sodales. Vivamus nec tincidunt nibh, eget pulvinar sem. Vestibulum nec augue at dui vulputate tempor. Suspendisse pulvinar, ante in bibendum mollis, tellus arcu placerat nunc, eget gravida nibh felis nec orci. Suspendisse faucibus cursus pellentesque. Fusce porta sem id turpis posuere, et gravida felis ornare. Proin convallis mauris ipsum, nec consequat arcu ornare vitae. Donec eu elementum lorem. Duis finibus nulla eget ipsum iaculis vestibulum. Fusce euismod consectetur mauris et luctus. Fusce pellentesque at risus non gravida. Nunc maximus commodo ante, et tincidunt lorem. Praesent id sem at elit varius fringilla vel at ipsum. Ut ut laoreet dolor.
+            </p>
+        </>
     );
 }
 
