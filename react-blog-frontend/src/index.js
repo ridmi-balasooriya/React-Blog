703718dd15435b8f44c4b52506999e4d37c3b7cf
ridmi-balasooriya <ridmi.bizy@gmail.com>
2023-03-13 12:26:41 +0400

    Setup authentication using FireBase. 1. index.js: configur and initialize FireBase authentication. 2. create LoginPage, CreateAccountPagein pages folder and useUser in hooks folder. 3. Implement LoginPage. 4. Update App.css file for relavent login styles.

--- react-blog-frontend/src/index.js
+++ react-blog-frontend/src/index.js
@@ -3,6 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+/*  Firebase configuration & initialization */
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCOBJ-ZDknWqI7ghSlFp8v5wVPaKQwzqhE",
+  authDomain: "react-blog-30c24.firebaseapp.com",
+  projectId: "react-blog-30c24",
+  storageBucket: "react-blog-30c24.appspot.com",
+  messagingSenderId: "934038767874",
+  appId: "1:934038767874:web:3b93d259be918e500e1a1b"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+/* End of Firebase configuration & initialization */
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ react-blog-frontend/src/pages/LoginPage.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword} from 'firebase/auth';
+
+const LoginPage = () => {
+    const [ email, setEmail ] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ error, setError ]  = useState('');
+    const navigate = useNavigate();
+
+    const logIn = async() => {
+        try{
+            await signInWithEmailAndPassword(getAuth(), email, password);
+            navigate('/articles');
+        }catch(e){
+            setError(e.message);
+        }
+    }
+
+
+    return(
+        <>
+            <h1>Login Page</h1>            
+            <div className="login-form">
+                {error && <div className="error">{error}</div>}
+                <input placeholder="Your email address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input placeholder="Your Password" type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className="button" onClick={logIn}>Log In</button>
+                <Link to='/create-account'>Don't have an account? Create on here.</Link>
+            </div>
+        </>
+        
+    );
+}
+
+export default LoginPage;
